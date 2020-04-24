e852717a0be92bbd094c8ce6c50bfd730737e728
kquiroz <39870381+kquiroz@users.noreply.github.com>
2020-04-24 10:28:54 -0500

    Update medicina_filiacion.py

--- mstech_estarbien/models/medicina_filiacion.py
+++ mstech_estarbien/models/medicina_filiacion.py
@@ -18,7 +18,7 @@ class MedicinaFiliacion(models.Model) :
             record.paciente_perfil_id = record.paciente_id.perfil_id
     
     fecha_cita = fields.Date(string='''Fecha de atención''')
-    tipo_cita = fields.Selection(string='''Tipo''',selection='''[["diagnostico","Diagnóstico"],["triaje","Triaje"],["antecedentes_personales","Antecedentes Personales"],["historia_ocupacional","Historia Ocupacional"],["medicina","Medicina general"],["dermatologia","Dermatología"],["grandes_alturas","Anexo N°16 A"],["altura","Certificación de trabajo en altura"],["confinados","Trabajo en espacios confinados"],["electrocardiograma","Electrocardiograma"],["oftalmologia","Oftalmología"],["audiometria","Audiometría"],["psicologia","Psicología"],["odontograma","Odontograma"],["musculo_esqueletico","Músculo Esquelético"],["radiologia","Radiología"],["espirometria","Espirometría"],["tsr","Test de Sintomático Respiratorio"],["laboratorio","Laboratorio"],["consentimiento","Consentimiento"],["interconsultas","Interconsultas"],["encuesta","Encuesta"]]''')
+    tipo_cita = fields.Selection(string='''Tipo''',selection='''[("diagnostico","Diagnóstico"),("triaje","Triaje"),("antecedentes_personales","Antecedentes Personales"),("historia_ocupacional","Historia Ocupacional"),("medicina","Medicina general"),("dermatologia","Dermatología"),("grandes_alturas","Anexo N°16 A"),("altura","Certificación de trabajo en altura"),("confinados","Trabajo en espacios confinados"),("electrocardiograma","Electrocardiograma"),("oftalmologia","Oftalmología"),("audiometria","Audiometría"),("psicologia","Psicología"),("odontograma","Odontograma"),("musculo_esqueletico","Músculo Esquelético"),("radiologia","Radiología"),("espirometria","Espirometría"),("tsr","Test de Sintomático Respiratorio"),("laboratorio","Laboratorio"),("consentimiento","Consentimiento"),("interconsultas","Interconsultas"),("encuesta","Encuesta")]''')
     #["diagnostico","Diagnóstico"]
     #["triaje","Triaje"]
     #["antecedentes_personales","Antecedentes Personales"]
@@ -44,11 +44,11 @@ class MedicinaFiliacion(models.Model) :
     atencion_inicio = fields.Datetime(string='''Inicio de la atención''')
     atencion_fin = fields.Datetime(string='''Fin de la atención''')
     
-    paciente_apellidos_nombres = fields.Char(string='''Apellidos y nombres''', compute='''_compute_paciente_datos''', store=True, readonly=True)
-    paciente_cargo = fields.Char(string='''Cargo''', compute='''_compute_paciente_datos''', store=True, readonly=True)
-    paciente_dni = fields.Char(string='''DNI''', compute='''_compute_paciente_datos''', store=True, readonly=True)
-    paciente_edad = fields.Char(string='''Edad''', compute='''_compute_paciente_datos''', store=True, readonly=True)
-    paciente_sexo = fields.Selection(string='''Sexo''',selection='''[['m', 'Male'], ['f', 'Female']]''', compute='''_compute_paciente_datos''', store=True, readonly=True)
+    #paciente_apellidos_nombres = fields.Char(string='''Apellidos y nombres''', compute='''_compute_paciente_datos''', store=True, readonly=True)
+    #paciente_cargo = fields.Char(string='''Cargo''', compute='''_compute_paciente_datos''', store=True, readonly=True)
+    #paciente_dni = fields.Char(string='''DNI''', compute='''_compute_paciente_datos''', store=True, readonly=True)
+    #paciente_edad = fields.Char(string='''Edad''', compute='''_compute_paciente_datos''', store=True, readonly=True)
+    #paciente_sexo = fields.Selection(string='''Sexo''',selection='''[('m', 'Male'), ('f', 'Female')]''', compute='''_compute_paciente_datos''', store=True, readonly=True)
 
     auditado = fields.Boolean(string='''Auditado''')
     auditor = fields.Many2one(string='''Auditor''')
@@ -60,6 +60,6 @@ class MedicinaFiliacion(models.Model) :
     diagnostico_ids = fields.One2many(comodel_name='''medicina.cita.diagnostico''', inverse_name='''cita_id''', string='''Diagnóstico''', domain=[('tipo_diagnostico_oido','=','bilateral')])
     diagnostico_oido_derecho_ids = fields.One2many(comodel_name='''medicina.cita.diagnostico''', inverse_name='''cita_id''', string='''Diagnóstico del oido derecho''', domain=[('tipo_diagnostico_oido','=','derecho')])
     diagnostico_oido_izquierdo_ids = fields.One2many(comodel_name='''medicina.cita.diagnostico''', inverse_name='''cita_id''', string='''Diagnóstico del oido izquierdo''', domain=[('tipo_diagnostico_oido','=','izquierdo')])
-    historial_paciente = fields.Many2many(string='''Historial del paciente''')
+    historial_paciente = fields.Many2many(comodel_name='''medicina.cita''', relation='''medicina_filiacion_medicina_cita_rel''', column1='''filiacion_id''', column2='''cita_id''', string='''Historial del paciente''')
     
     name = fields.Char(string='''Filiación''')

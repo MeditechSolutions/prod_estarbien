cc2b08ec921ddefe45638216add794ed79f77263
kquiroz <39870381+kquiroz@users.noreply.github.com>
2020-05-07 18:41:23 -0500

    Add files via upload

--- mouse_ruc_validation/__init__.py
+++ mouse_ruc_validation/__init__.py
@@ -6,3 +6,4 @@ from odoo import api, SUPERUSER_ID
 def l10n_latam_base_view_partner_latam_form_active(cr, registry) :
     env = api.Environment(cr, SUPERUSER_ID, {})
     env.ref('l10n_latam_base.view_partner_latam_form').write({'active': True})
+    env.ref('base_vat.view_partner_form').write({'active': True})
